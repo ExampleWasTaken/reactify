b7fc0224d9ea6c70bbaa2637960a0a9f72b482f7
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 01:17:36 +1300

    fix: header no clickable

--- src/components/web/header/Header.tsx
+++ src/components/web/header/Header.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import { routes } from '../../../utils/routes.ts';
+
+// FIXME: when scrolling all the way down the header scrolls outside the viewport when it should stay there regardless of the scroll position
 export const Header = () => {
   return (
     <header className="sticky top-0 px-8 w-full h-20 flex flex-col justify-center bg-green text-white border-b">
-      <div className="font-bold text-3xl">Reactify&trade;</div>
+      <h1 className="font-bold text-3xl">
+        <Link
+          to={routes.web.root}
+          className="font-bold text-3xl text-white no-underline"
+        >
+          Reactify&trade;
+        </Link>
+      </h1>
     </header>
   );
 };
