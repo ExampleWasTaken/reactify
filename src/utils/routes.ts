d30b6726f175676356dae01333657fdcb7cdcdda
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 01:08:35 +1300

    fix: put web routes into their own object in routes.ts

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,4 +1,10 @@
 export const routes = {
+  web: {
+    landingPage: '/',
+    documentation: '/documentaion',
+    faq: '/faq',
+  },
+  faq: '/faq',
   app: '/app',
   home: '/app/home',
   search: '/app/search',
