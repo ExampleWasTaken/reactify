47dcd237fb36bbdf366242790ab13610be049d60
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 00:38:34 +1300

    fix: auto redirect to app home view when navigating to /app

--- src/components/app/root/AppRoot.tsx
+++ src/components/app/root/AppRoot.tsx
@@ -1,13 +1,14 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { AppNavbar } from '../global/navbar/AppNavbar.tsx';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { routes } from '../../../utils/routes.ts';
 import { useEffect } from 'react';
+import { AppNavbar } from '../global/navbar/AppNavbar.tsx';
 
 export const AppRoot = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    navigate(routes.home);
+    location.pathname.endsWith(routes.app) && navigate(routes.home);
   }, []);
 
   return (
