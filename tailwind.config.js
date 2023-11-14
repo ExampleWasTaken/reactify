4ab19f008479969b88e817841a3396d3bf02bb08
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 20:25:05 +1300

    fix: tailwind colors not extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
       './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      'green': '#1db954',
-      'purple': '#400073',
-      'white': '#ffffff',
-      'black': '#191414',
+    extend: {
+      colors: {
+        'green': '#1db954',
+        'purple': '#400073',
+        'white': '#ffffff',
+        'black': '#191414',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
