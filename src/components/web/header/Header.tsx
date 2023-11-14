3119da59a4a97882d17b45611c7699e42daa3e6a
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 20:23:28 +1300

    fix: header border too bright

--- src/components/web/header/Header.tsx
+++ src/components/web/header/Header.tsx
@@ -4,7 +4,7 @@ import { routes } from '../../../utils/routes.ts';
 // FIXME: when scrolling all the way down the header scrolls outside the viewport when it should stay there regardless of the scroll position
 export const Header = () => {
   return (
-    <header className="sticky top-0 px-8 w-full h-20 flex flex-col justify-center bg-green text-white border-b">
+    <header className="sticky top-0 px-8 w-full h-20 flex flex-col justify-center bg-green text-white border-b border-white/50">
       <h1 className="font-bold text-3xl">
         <Link
           to={routes.web.root}
