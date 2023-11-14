eabb34765143407b560dca3712dd96c662873c40
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-11-14 22:26:28 +1300

    feat: add clsx

--- src/components/shared/PrimaryButton.tsx
+++ src/components/shared/PrimaryButton.tsx
@@ -1,23 +1,30 @@
 import React, { ReactNode } from 'react';
+import { clsx } from 'clsx';
 
 interface PrimaryButtonProps {
   className: string;
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  disabled?: boolean;
   children: ReactNode;
 }
 
 export const PrimaryButton = ({
   className,
   onClick,
+  disabled,
   children,
 }: PrimaryButtonProps) => {
-  const style =
-    'bg-green text-black rounded-lg hover:bg-green/90 active:bg-green/75 ' +
-    className;
+  const style = 'bg-green text-black rounded-lg ' + className;
   return (
     <button
-      className={style}
+      className={clsx(
+        style,
+        disabled
+          ? 'opacity-50 cursor-not-allowed'
+          : 'hover:bg-green/90 active:bg-green/75'
+      )}
       onClick={event => onClick(event)}
+      disabled={disabled}
     >
       {children}
     </button>
