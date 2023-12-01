54831ac20f6c2d1a11fd3de9944afefb6bfe07da
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-12-01 14:13:14 +0100

    feat: apply blur to navbar

--- src/components/app/global/navbar/AppNavbar.tsx
+++ src/components/app/global/navbar/AppNavbar.tsx
@@ -12,7 +12,7 @@ export const AppNavbar = () => {
   };
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full py-4 px-8 bg-gradient-to-t from-[#000] flex flex-row justify-around">
+    <nav className="fixed bottom-0 left-0 w-full py-4 px-8 backdrop-blur-xl backdrop-brightness-50 flex flex-row justify-around">
       <IconContext.Provider value={{ size: '28' }}>
         <NavLink
           to={routes.app.home}
