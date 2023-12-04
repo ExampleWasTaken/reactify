560448558847274eb4649c59aaa32ace7c12e2d2
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-12-04 04:00:51 +0100

    chore: temporarily disable playback sdk

--- src/components/app/AppRoot.tsx
+++ src/components/app/AppRoot.tsx
@@ -2,16 +2,15 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { routes } from '../../utils/routes.ts';
 import { useEffect, useState } from 'react';
 import { AppFooter } from './global/footer/AppFooter.tsx';
-import { SpotifyWebSDK } from 'spotify-web-playback-sdk-for-react';
 import { useSpotify } from '../../hooks/useSpotify.tsx';
-import { FullscreenSpinner } from './global/loaders/FullscreenSpinner.tsx';
 
 export const AppRoot = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const spotify = useSpotify();
 
-  const [accessToken, setAccessToken] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
     if (location.pathname.endsWith('app/') || location.pathname.endsWith('app')) {
@@ -31,7 +30,7 @@ export const AppRoot = () => {
       className="w-screen h-screen bg-black text-white overflow-scroll"
       id="app-container"
     >
-      {accessToken ? (
+      {/*{accessToken ? (
         <SpotifyWebSDK
           name="Reactify"
           getOAuthToken={cb => cb(accessToken)}
@@ -41,7 +40,9 @@ export const AppRoot = () => {
         </SpotifyWebSDK>
       ) : (
         <FullscreenSpinner />
-      )}
+      )}*/}
+      <Outlet />
+      <AppFooter />
     </div>
   );
 };
