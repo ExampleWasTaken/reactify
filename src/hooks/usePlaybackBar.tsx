93ff529816cccd2d8e0b00652ad325522adac21a
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-12-09 04:17:04 +0100

    docs: fix typo

--- src/hooks/usePlaybackBar.tsx
+++ src/hooks/usePlaybackBar.tsx
@@ -11,7 +11,7 @@ export const usePlaybackBar = () => {
   /**
    * Calculates the progress of playback while adjusting for time passed between fetching and now. <br/>
    * **The timestamp MUST be synchronized to the provided progress value!** Note, that the Spotify API does not return
-   * synchronize those two values so you will need to keep track of the timestamp of last fetched data yourself!
+   * synchronize those two values, so you will need to keep track of the timestamp of last fetched data yourself!
    * @param timestamp_ms The time in milliseconds when the data was fetched.
    * @param progress_ms The progress in milliseconds into the current track.
    * @param duration_ms The duration in milliseconds of the current track.
