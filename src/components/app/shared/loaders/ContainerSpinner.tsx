3352db5b0f8f6a462e2780ea0b75d3a5206bb5d5
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-12-23 20:08:34 +0100

    feat: add label to spinners

--- src/components/app/shared/loaders/ContainerSpinner.tsx
+++ src/components/app/shared/loaders/ContainerSpinner.tsx
@@ -3,14 +3,26 @@ import { useState } from 'react';
 
 interface ContainerSpinnerProps {
   className?: string;
+  label?: string;
+  labelColor?: string;
 }
 
-export const ContainerSpinner = ({ className }: ContainerSpinnerProps) => {
-  const [style] = useState('text-green flex justify-center items-center ' + className);
+export const ContainerSpinner = ({ className, label, labelColor }: ContainerSpinnerProps) => {
+  const [spinnerStyle] = useState('text-green flex justify-center items-center ' + className);
 
   return (
-    <div className={style}>
-      <Spinner />
+    <div className={spinnerStyle}>
+      <div className="flex flex-col max-w-[66.6%] justify-center items-center space-y-3">
+        <Spinner />
+        {label && (
+          <p
+            className="text-white text-center font-light"
+            style={{ color: labelColor }}
+          >
+            {label}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/app/shared/loaders/FullscreenSpinner.tsx
+++ src/components/app/shared/loaders/FullscreenSpinner.tsx
@@ -1,9 +1,16 @@
 import { Spinner } from './Spinner.tsx';
 
-export const FullscreenSpinner = () => {
+interface FullscreenSpinnerProps {
+  label?: string;
+}
+
+export const FullscreenSpinner = ({ label }: FullscreenSpinnerProps) => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-black text-green flex justify-center items-center">
-      <Spinner />
+      <div className="flex flex-col max-w-[66.6%] justify-center items-center space-y-3">
+        <Spinner />
+        {label && <p className="text-white text-center font-light">{label}</p>}
+      </div>
     </div>
   );
 };
